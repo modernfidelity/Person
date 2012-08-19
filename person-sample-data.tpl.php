9cb142adc5489bcfb0bfdbf265478c591eea2507
modernfidelity <mark@modernfidelity.co.uk>
2012-08-19 09:16:01 +0100

    mv readme to md

--- person-sample-data.tpl.php
+++ person-sample-data.tpl.php
@@ -2,16 +2,16 @@
 
 /**
  * @file
- * Example tpl file for theming a single model-specific theme
+ * Example tpl file for theming a single person-specific theme
  *
  * Available variables:
  * - $status: The variable to theme (while only show if you tick status)
  * 
  * Helper variables:
- * - $model: The Model object this status is derived from
+ * - $person: The person object this status is derived from
  */
 ?>
 
-<div class="model-status">
-  <?php print '<strong>Model Sample Data:</strong> ' . $model_sample_data = ($model_sample_data) ? 'Switch On' : 'Switch Off' ?>
+<div class="person-status">
+  <?php print '<strong>Person Sample Data:</strong> ' . $person_sample_data = ($person_sample_data) ? 'Switch On' : 'Switch Off' ?>
 </div>
